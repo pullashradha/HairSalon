e991d0337389df866b340361ef5e1d54ef1951c0
Shradha Pulla <pullashradha@gmail.com>
2016-07-15 07:15:38 -0600

    Create getters and setters for properties in Stylist.cs, create empty database test

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -6,6 +6,38 @@ namespace HairSalon
 {
   public class Stylist
   {
-
+    private int _id;
+    private string _name;
+    private string _phoneNumber;
+    public Stylist (string Name, string PhoneNumber, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _phoneNumber = PhoneNumber;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+    public static List<Stylist> GetAll()
+    {
+      
+    }
   }
 }
--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -6,11 +6,17 @@ using Xunit;
 
 namespace HairSalon
 {
-  public class StylistTest : IDisposable
+  public class StylistTest
   {
     public StylistTest()
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Test_Empty_GetAllStylists()
+    {
+      Stylist newStylist = new Stylist ("Stylist Name", "Stylist's Phone Number");
+      Assert.Equal(0, newStylist.GetAll());
     }
   }
 }
