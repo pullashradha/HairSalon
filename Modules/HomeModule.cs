8ccb2d13af1bf480130a9865e764b5d2f05af9de
Shradha Pulla <pullashradha@gmail.com>
2016-07-15 09:58:08 -0600

    Add confirmation page after adding a new stylist, styles for all pages besides index.cshtml are not working

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,6 +9,15 @@ namespace HairSalon
       Get ["/"] = _ => {
         return View ["index.cshtml", Stylist.GetAll()];
       };
+      Post ["/stylist/new/created"] = _ => {
+        Stylist newStylist = new Stylist
+        (
+          Request.Form ["stylist-name"],
+          Request.Form ["stylist-phone-number"]
+        );
+        newStylist.Save();
+        return View ["stylist_created.cshtml", newStylist];
+      };
     }
   }
 }
