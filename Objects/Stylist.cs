8f76eab0c8d60b8e21167b3e19c623c0bebeea28
Shradha Pulla <pullashradha@gmail.com>
2016-07-17 15:51:56 -0600

    Remove @StylistId from SqlCommand in DeleteOne() method to make it the same as the searchId-drier code, in Stylist.cs

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -240,15 +240,11 @@ namespace HairSalon
     {
       SqlConnection conn = DB.Connection();
       conn.Open();
-      SqlCommand cmd = new SqlCommand ("DELETE FROM stylists WHERE id = @SearchId; DELETE FROM clients WHERE stylist_id = @StylistId;", conn);
+      SqlCommand cmd = new SqlCommand ("DELETE FROM stylists WHERE id = @SearchId; DELETE FROM clients WHERE stylist_id = @SearchId;", conn);
       SqlParameter idParameter = new SqlParameter();
       idParameter.ParameterName = "@SearchId";
       idParameter.Value = searchId;
-      SqlParameter stylistIdParameter = new SqlParameter();
-      stylistIdParameter.ParameterName = "@StylistId";
-      stylistIdParameter.Value = searchId;
       cmd.Parameters.Add(idParameter);
-      cmd.Parameters.Add(stylistIdParameter);
       cmd.ExecuteNonQuery();
     }
     public static void DeleteAll()
